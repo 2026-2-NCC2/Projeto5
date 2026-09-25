5338536f5b1192f03d7048b0bab9090a7cdfff97
Sabrina Nascimento <sabrinanascimento2004@gmail.com>
2026-09-24 22:52:55 -0300

    configura pagina inicial

--- src/Entrega 1/Frontend/src/App.jsx
+++ src/Entrega 1/Frontend/src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react';
 import Cabecalho from './components/Cabecalho';
 import Rodape from './components/Rodape';
+import Home from './paginas/Home';
+import Login from './paginas/Login';
+import Cadastro from './paginas/Cadastro';
+import Dashboard from './paginas/Dashboard';
+import Eventos from './paginas/Eventos';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
     <>
       <Cabecalho/>
+      {/* Define as rotas para navegação na plataforma */}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/> } />
+          <Route path='/login' element={<Login/> } />
+          <Route path='/cadastro' element={<Cadastro/> } />
+          <Route path='/dashboard' element={<Dashboard/> } />
+          <Route path='/eventos' element={<Eventos/> } />
+        </Routes>
+      </BrowserRouter>
       <Rodape/>
     </>
   )
---
+++ src/Entrega 1/Frontend/src/paginas/Eventos.jsx
@@ -0,0 +1,7 @@
+
+function Eventos() {
+    return (
+        <h1>qualquer coisa</h1>
+    );
+}
+export default Eventos;
